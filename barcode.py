e08bf1508db8541f6986b662555acca785ee0cd7
Herman <herman.bastesen@outlook.com>
2019-09-05 21:24:34 +0200

    Optional samples per second argument

--- barcode.py
+++ barcode.py
@@ -7,19 +7,22 @@ def averageColorPerRow(image):
     a =  np.average(image, axis=1)
     return a[:, np.newaxis, :]
 
-def convert(inputfile, outputfile, fps):
+def convert(inputfile, outputfile, sps):
     vid = cv2.VideoCapture(inputfile)
     length = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
-    vidHeight = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT) )
+    framerate = int(vid.get(cv2.CAP_PROP_FPS))
+    samplerate = int(framerate / sps) if sps > 0 else 1
 
     columns = [] #Will hold bgr formatted columns of output image
 
+    counter = 0
     success, image = vid.read()
     while success:
-        columns.append(averageColorPerRow(image))
-        
-        print("Progress: {}/{} ({:.1f}%)".format(len(columns), length, (100*len(columns)/length)), end="\r")
+        if counter % samplerate == 0:
+            columns.append(averageColorPerRow(image))
+        print("Progress: {}/{} ({:.1f}%)".format(counter, length, (100*counter/length)), end="\r")
         success, image = vid.read()
+        counter += 1
 
     #Assemble columns and save image:
     cv2.imwrite(outputfile, cv2.hconcat(columns))
@@ -33,22 +36,22 @@ if __name__ == "__main__":
         print("""barcode.py
 
 Usage:
-    barcode.py <inputfile> [outputfile] [--fps=<fps>]
+    barcode.py <inputfile> [outputfile] [--sps=<sps>]
     barcode.py -h | --help
 
 Options:
     -h --help     Show this information
     [outputfile]  Set output filename
-    --fps=<fps>   How many frames to sample from each second of video
+    --sps=<sps>   Samples Per Second - How many frames to sample from each second of video
 
 Examples:
-    barcode.py video.mp4 out.png --fps=4""")
+    barcode.py video.mp4 out.png --sps=4""")
     else:
         parser = argparse.ArgumentParser()
 
         parser.add_argument("inputfile", type=str)
         parser.add_argument("outputfile", type=str, nargs='?', default="output.png")
-        parser.add_argument("--fps", type=int, default=0)
+        parser.add_argument("--sps", type=int, default=0)
 
         pArgs = parser.parse_args()
-        convert(pArgs.inputfile, pArgs.outputfile, pArgs.fps)
+        convert(pArgs.inputfile, pArgs.outputfile, pArgs.sps)
